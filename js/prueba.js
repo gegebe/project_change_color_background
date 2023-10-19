b167abbef4f25053d2102d20b045eb8981f5c606
gegebe <ggirbes@gmail.com>
2023-10-19 09:42:26 +0200

    finished

---
+++ js/prueba.js
@@ -0,0 +1,11 @@
+let conFloor = Math.floor((Math.random() * 6));
+console.log(conFloor);
+// Igual o menor
+
+let conCeil = Math.ceil((Math.random() * 6));
+console.log(conCeil);
+// Igual o mayor
+
+let conParseInt = parseInt((Math.random() * 6));
+console.log(conParseInt);
+// Solo la parte entera
--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 //En este array de strings, tenemos todos los colores
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
+let button = document.querySelector("button");
+button.addEventListener("click", changeColor);
 
+function changeColor() {
+
+  let body = document.querySelector("body");
+  let color;
+
+    color = Math.floor((Math.random() * 6));
+    body.style.backgroundColor = colors[color];
+  
+
+}
